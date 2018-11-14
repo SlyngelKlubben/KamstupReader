30cd3cbd8174a1a718a07dd91f55d71a6766bb17
Thomas Poulsen <thomas@lha66.dk>
2018-11-14 20:37:23 +0100

    small change

--- Dashboard/hus.R
+++ Dashboard/hus.R
@@ -7,7 +7,7 @@ library(httr)
 library(plyr)
 library(lubridate)
 library(lattice)
-d1 <- GET("http://192.168.0.47:3000/hus/public/tyv")
+d1 <- GET("http://192.168.0.47:3000/hus/public/tyv") ## 47, 16
 d2 <- content(d1)
 d3 <- ldply(d2, as.data.frame)
 Pat <- '^(\\S+)\\s+(\\d+)$'
@@ -15,7 +15,7 @@ d4 <- transform(d3, Source=sub(Pat,'\\1',as.character(content)), Value=as.numeri
 d5 <- transform(d4, TimeDiff=c(NA, diff(Time)))
 d6 <- transform(d5, perMinute=60*60/TimeDiff)
 
-with(subset(d5), xyplot(Value~Time, scales=list(rot=90), type=c('h','p')))
+with(subset(d5), xyplot(Value~Time | Source, scales=list(rot=90, x="free",y="free"), type=c('h','p')))
 with(subset(d6, id>5300), xyplot(perMinute~Time, scales=list(rot=90), type=c('h','p'), main="Strømforbrug i W"))
 
 write.csv(file=sprintf("data1_%s.csv", Sys.Date()), d6)
