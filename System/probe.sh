b04d15f4fd0f61d58b603b18b8c1b19777309d00
Thomas Poulsen <thomas@lha66.dk>
2018-12-30 17:10:36 +0100

    make ip an option

--- System/probe.sh
+++ System/probe.sh
@@ -1,2 +1,7 @@
-curl  -H "Content-Type: application/json" -X GET http://192.168.0.200:3000/hus/public/tyv | \
+IP=$1
+if [[ $IP == "" ]] ; then
+    IP="192.168.0.200"
+fi
+echo "IP=$IP"
+curl  -H "Content-Type: application/json" -X GET http://${IP}:3000/hus/public/tyv | \
  jq -r '(map(keys)  | add | unique) as $cols | map(. as $row | $cols | map($row[.])) as $rows | $cols, $rows[] | @csv'
