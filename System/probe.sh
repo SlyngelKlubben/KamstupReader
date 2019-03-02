fd94a7e87f8647f5f48d05f837a6dbcfe717278e
Thomas Poulsen <thomas@lha66.dk>
2019-03-02 14:02:45 +0100

    add dbanme as option

--- System/probe.sh
+++ System/probe.sh
@@ -1,7 +1,12 @@
 IP=$1
+DB=$2
 if [[ $IP == "" ]] ; then
     IP="192.168.0.200"
 fi
+if [[ $DB == "" ]] ; then
+    DB="tyv"
+fi
 echo "IP=$IP"
+echo "DB=$DB"
 curl  -H "Content-Type: application/json" -X GET http://${IP}:3000/hus/public/tyv | \
  jq -r '(map(keys)  | add | unique) as $cols | map(. as $row | $cols | map($row[.])) as $rows | $cols, $rows[] | @csv'
