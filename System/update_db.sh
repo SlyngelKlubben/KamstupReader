21755054442fa717bc94722c60ff8718c71e2d89
Thomas Poulsen <thomas@lha66.dk>
2019-09-08 21:26:22 +0200

    just do it

--- System/update_db.sh
+++ System/update_db.sh
@@ -3,14 +3,19 @@ DBUSER="iot"
 DBPW="iot" ## password
 DB="hus"
 
-### El
-## ref: https://stackoverflow.com/a/34518648
-for Col in content software_version content power_w intensity threshold signal IP MAC senid ; do 
-##    if psql -U iot -h 192.168.1.3 hus -tAc \
-    if sudo -u postgres psql postgres -tAc \
-       "SELECT EXISTS (SELECT 1 FROM information_schema.columns WHERE table_schema='public' AND table_name='el' AND column_name='$Col');" | grep 'f'; then
-	echo "No $Col in El"
-    else
-	echo "El has $Col"
-    fi
-done
+## Adding existing columns just fails
+## Fields:  content software_version content power_w intensity threshold signal IP MAC senid 
+
+PGPASSWORD=$DBPW psql -U $DBUSER $DB -tAc "ALTER TABLE el ADD COLUMN software_version text;"
+
+PGPASSWORD=$DBPW psql -U $DBUSER $DB -tAc "ALTER TABLE el ADD COLUMN power_w text;"
+
+PGPASSWORD=$DBPW psql -U $DBUSER $DB -tAc "ALTER TABLE el ADD COLUMN threshold integer  text;"
+
+PGPASSWORD=$DBPW psql -U $DBUSER $DB -tAc "ALTER TABLE el ADD COLUMN signal integer  text;"
+
+PGPASSWORD=$DBPW psql -U $DBUSER $DB -tAc "ALTER TABLE el ADD COLUMN signal MAC  text;"
+
+
+
+
