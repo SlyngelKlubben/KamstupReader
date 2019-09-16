0d44075d901f31df7ae2254ecdb81b60a9f84b44
Thomas Poulsen <thomas@lha66.dk>
2019-09-16 20:29:40 +0200

    update vand

--- System/update_db.sh
+++ System/update_db.sh
@@ -31,4 +31,15 @@ PGPASSWORD=$DBPW psql -U $DBUSER $DB -tAc "ALTER TABLE el ADD COLUMN timer_perio
 ## Error: pq: column "IP" of relation "el" does not exist
 ## [negroni] Completed 400 Bad Request in 3.659251ms
 
-
+## vand
+PGPASSWORD=$DBPW psql -U $DBUSER $DB -tAc "ALTER TABLE vand ADD COLUMN software_version text;"
+PGPASSWORD=$DBPW psql -U $DBUSER $DB -tAc "ALTER TABLE vand ADD COLUMN flow_l_per_min text;"
+PGPASSWORD=$DBPW psql -U $DBUSER $DB -tAc "ALTER TABLE vand ADD COLUMN threshold integer;"
+PGPASSWORD=$DBPW psql -U $DBUSER $DB -tAc "ALTER TABLE vand ADD COLUMN signal integer ;"
+PGPASSWORD=$DBPW psql -U $DBUSER $DB -tAc "ALTER TABLE vand ADD COLUMN intensity integer ;"
+PGPASSWORD=$DBPW psql -U $DBUSER $DB -tAc "ALTER TABLE vand ADD COLUMN cycle_count integer ;"
+PGPASSWORD=$DBPW psql -U $DBUSER $DB -tAc "ALTER TABLE vand ADD COLUMN \"IP\" text ;"
+PGPASSWORD=$DBPW psql -U $DBUSER $DB -tAc "ALTER TABLE vand ADD COLUMN \"MAC\" text;"
+PGPASSWORD=$DBPW psql -U $DBUSER $DB -tAc "ALTER TABLE vand ADD COLUMN time_ms bigint;"
+PGPASSWORD=$DBPW psql -U $DBUSER $DB -tAc "ALTER TABLE vand ADD COLUMN delta_ms bigint;"
+PGPASSWORD=$DBPW psql -U $DBUSER $DB -tAc "ALTER TABLE vand ADD COLUMN timer_periods int;"
