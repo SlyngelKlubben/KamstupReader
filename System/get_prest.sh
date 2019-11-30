ebde5212b241830193ae5e67a5d54c22945a693b
Thomas Poulsen <thomas@lha66.dk>
2019-11-30 15:52:34 +0000

    update rc.local

--- System/get_prest.sh
+++ System/get_prest.sh
@@ -4,8 +4,7 @@
 
 ## Check if Prest is alredy there
 if [[ -e /opt/Prest/prest-linux-arm-7 ]] ; then
-    echo "PREST already installed. Exiting"
-    exit
+    echo "PREST already installed."
 else 
 ## Download Prest
     wget https://github.com/prest/prest/releases/download/v0.3.0/prest-linux-arm-7
@@ -16,13 +15,12 @@ else
 fi
 
 ## Check if rc.local starts Prest
-if grep -q prest-linux-arm-7 rc.local ; then
+if grep -q "prest-linux-arm-7" /etc/rc.local
+then
     echo "rc.local starts prest"
 else
-    echo "Needs to update rc.local"
+    ## update rc.local
+    echo "Updating rc.local"
+    sudo sed -i '/^exit 0/i ## Run pRest server\ncd /opt/Prest/ && ./prest-linux-arm-7 > /opt/Prest/prest.log &\n\n' /etc/rc.local
 fi
 
-
-
-
-## update rc.local
