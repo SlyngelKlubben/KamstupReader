a4029e17c1c00777428ce35a7ce02dd083f4b3ba
Thomas Poulsen <thomas@lha66.dk>
2019-11-30 18:51:14 +0100

    add postgis to store sensor locations

--- System/add_gis.sh
+++ System/add_gis.sh
@@ -1,28 +1,32 @@
 ## add postgis
+DBUSER="iot"
+DBPW="iot" ## password
 DB="hus"
 
 
 
-## Update packages
-sudo apt update
-sudo apt upgrade
+# ## Update packages
+# sudo apt update
+# sudo apt upgrade
 
-## install postgis
-sudo apt install postgis
+# ## install postgis
+# sudo apt install postgis
 
-## restart postgres
-sudo service postgresql restart
+# ## restart postgres
+# sudo service postgresql restart
 
 ## Enable postgis
-sudo su - postgres
-psql $DB -tAc "CREATE EXTENSION postgis_topology CASCADE ;"
+#### sudo su - postgres
+sudo -u postgres psql $DB -tAc "CREATE EXTENSION postgis_topology CASCADE ;"
 
 ## Create sensor_location table
-psql $DB -tAc "CREATE TABLE sensor_location (
+sudo -u postgres psql $DB -tAc "CREATE TABLE public.sensor_location (
 id SERIAL PRIMARY KEY,
 mac TEXT NOT NULL,
 name TEXT NOT NULL,
-location GEOMETRY(POINTZ,25832),
+location GEOMETRY(POINTZ,25832), -- not null?
 start_date TIMESTAMPTZ  DEFAULT NOW() 
-) ;"
+) ;
+ALTER TABLE public.sensor_location OWNER to $DBUSER;
+GRANT ALL on TABLE public.${TBL} to $DBUSER;" 
 
