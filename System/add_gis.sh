8f209525832b0c79933ba06760c9c920a0a4d69b
Thomas Poulsen <thomas@lha66.dk>
2020-03-29 13:49:05 +0200

    table as var

--- System/add_gis.sh
+++ System/add_gis.sh
@@ -2,25 +2,24 @@
 DBUSER="iot"
 DBPW="iot" ## password
 DB="hus"
-
-
+TBL="sensor_location"
 
 # ## Update packages
-# sudo apt update
-# sudo apt upgrade
+sudo apt update
+sudo apt upgrade
 
 # ## install postgis
-# sudo apt install postgis
+sudo apt install postgis
 
 # ## restart postgres
-# sudo service postgresql restart
+sudo service postgresql restart
 
 ## Enable postgis
 #### sudo su - postgres
 sudo -u postgres psql $DB -tAc "CREATE EXTENSION postgis_topology CASCADE ;"
 
 ## Create sensor_location table
-sudo -u postgres psql $DB -tAc "CREATE TABLE public.sensor_location (
+sudo -u postgres psql $DB -tAc "CREATE TABLE public.${TBL} (
 id SERIAL PRIMARY KEY,
 mac TEXT NOT NULL,
 name TEXT NOT NULL,
