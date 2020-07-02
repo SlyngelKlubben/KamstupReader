a2f88eaa04c98947f674027116d45fb9db60a306
Thomas Poulsen <thomas@lha66.dk>
2020-07-02 21:51:00 +0200

    fix pinnig

--- System/060_relay_control.sh
+++ System/060_relay_control.sh
@@ -16,13 +16,13 @@ PGPASSWORD=$DBPW psql -U $DBUSER $DB -tAc "CREATE TABLE public.relay_control ( i
 
 PGPASSWORD=$DBPW psql -U $DBUSER $DB -tAc "CREATE OR REPLACE FUNCTION public.relay_state (
 task text , 
-now_time time with time zone ,
+now_time timestamp with time zone ,
 off_start_time time with time zone ,
 off_end_time time with time zone ,
 current_light double precision,
 off_light_level double precision,
 pin_state text,
-pin_expire time with time zone
+pin_expire timestamp with time zone
 )
 RETURNS text AS
 $$
@@ -32,7 +32,7 @@ BEGIN
   END IF;
     IF task = 'light_control' THEN
       IF off_start_time <  off_end_time THEN
-        IF ( now_time > off_start_time and now_time < off_end_time ) 
+        IF ( now_time::time with time zone > off_start_time and now_time::time with time zone < off_end_time ) 
           OR current_light > off_light_level
         THEN
           RETURN 'off';
@@ -40,7 +40,7 @@ BEGIN
           RETURN 'on';
         END IF ;
       ELSE
-        IF ( now_time > off_start_time or now_time < off_end_time ) 
+        IF ( now_time::time with time zone > off_start_time or now_time::time with time zone < off_end_time ) 
             OR current_light > off_light_level
         THEN
             RETURN 'off';
@@ -67,7 +67,7 @@ sleep 3
 
 PGPASSWORD=$DBPW psql -U $DBUSER $DB -tAc 'CREATE OR REPLACE VIEW public.relay AS
 SELECT public.relay_state(
- task::text, now()::time with time zone, off_time_start::time with time zone, off_time_end::time with time zone, envi.light::double precision, off_light_level::double precision, pin_state::text, pin_expire::time with time zone) 
+ task::text, now()::timestamp with time zone, off_time_start::time with time zone, off_time_end::time with time zone, envi.light::double precision, off_light_level::double precision, pin_state::text, pin_expire::timestamp with time zone) 
 AS state, relay_control.relay_mac, off_time_start, off_time_end, off_light_level,
 envi.light as current_light, envi.id as envi_id, relay_control.id as relay_id,
 envi."MAC" as envi_mac,
