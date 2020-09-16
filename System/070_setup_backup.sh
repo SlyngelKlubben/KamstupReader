add0ae42c0f20a5d4c455fbdb0c875ced97c91a1
Thomas Poulsen <thomas@lha66.dk>
2020-09-16 21:27:28 +0200

    crontab instructions

--- System/070_setup_backup.sh
+++ System/070_setup_backup.sh
@@ -1,14 +1,20 @@
 ## make sure we have nas mounted on /tmp/NAS/ and have a writeable folder Backup there
 
+## crontab
+# m h  dom mon dow   command
+# 0 0 * * * /usr/bin/pg_dump -d hus  -Fc > "/tmp/NAS/daily.pgdump"
+# 1 1 * * * /bin/bash /home/pi/KamstupReader/System/backup.sh
+
+
 NAS="192.168.0.100"
 
 sudo apt install nfs-common
 
 sudo cat <<EOF >> /etc/rc.local
-sudo mkdir /tmp/NAS
-sudo mount  -t nfs ${NAS}:/volume1/iot /tmp/NAS
-sudo chown pi /tmp/NAS
-sudo chgrp -R postgres /tmp/NAS
-sudo chmod -R ug+rwX /tmp/NAS
+mkdir /tmp/NAS
+mount  -t nfs ${NAS}:/volume1/iot /tmp/NAS
+chown pi /tmp/NAS
+chgrp -R postgres /tmp/NAS
+chmod -R ug+rwX /tmp/NAS
 EOF
 
